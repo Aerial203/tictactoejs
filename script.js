8daab3b9c75f58bf09ee445c00247f85e541dde4
Ankit Shrestha <ankit.shrestha196@gmail.com>
2022-07-16 23:38:29 +0545

    Add box-shadow complete style

---
+++ script.js
@@ -0,0 +1,9 @@
+// const PLAYER1 = "X";
+// const PLAYER2 = "0";
+
+// const textSymbol = document.querySelector(".symbol");
+
+// document.addEventListener("click", e => {
+//     if (!e.target.matches(".symbol")) return
+//     textSymbol.textContent = PLAYER1
+// })
