266b0b219ed48030af0b8443fab616bdf02dbcaf
Ankit Shrestha <ankit.shrestha196@gmail.com>
2022-07-19 20:05:20 +0545

    Initial script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// const PLAYER1 = "X";
-// const PLAYER2 = "0";
+const MARK = ["X", "0"]
+let tracer = ""
+const textSymbol = document.querySelector(".inputText")
 
-// const textSymbol = document.querySelector(".symbol");
+// add mark: done
+// if already marked cannot add second mark in same place
+//check for winner
 
-// document.addEventListener("click", e => {
-//     if (!e.target.matches(".symbol")) return
-//     textSymbol.textContent = PLAYER1
-// })
+document.addEventListener("click", e => {
+    if (!e.target.matches(".symbol")) return
+
+    const childNode = e.target.children[0]
+    add_mark(childNode)
+}) 
+
+
+function add_mark(childNode) {
+    console.log(childNode)
+    if (!tracer){
+        let choosePlayer = Math.floor(Math.random() * 2) 
+        childNode.textContent = MARK[choosePlayer]
+        tracer = MARK[choosePlayer]
+    } else {
+        childNode.textContent = tracer == "X" ? tracer =  MARK[1] : tracer = MARK[0]
+    }
+}
